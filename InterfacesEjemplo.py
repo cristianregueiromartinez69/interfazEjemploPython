8a13ded374323cf582da20ac691cec9ee9cb892c
Cristian <cregueiromartinez@danielcastelao.org>
2024-10-25 13:18:31 +0200

    haciendo el primer boton

--- InterfacesEjemplo.py
+++ InterfacesEjemplo.py
@@ -6,7 +6,7 @@ import sys
 
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPalette, QColor
-from PyQt6.QtWidgets import (QMainWindow, QApplication)
+from PyQt6.QtWidgets import (QMainWindow, QApplication, QVBoxLayout, QPushButton, QWidget)
 from setuptools.extern import names
 
 
@@ -15,10 +15,17 @@ class PrimeraVentana(QMainWindow):
         super().__init__()
         self.setWindowTitle('Ventana ejemplo') #este metodo le da un titulo a la ventana
         self.setFixedSize(800, 600) #este metodo fija el tamaño de la ventana
-        paleta = self.palette()
-        paleta.setColor(QPalette.ColorRole.Window, QColor("purple"))
-        self.setPalette(paleta)
+        paleta = self.palette() #obtenemos el color de la paleta
+        paleta.setColor(QPalette.ColorRole.Window, QColor("purple")) #cambiamos el color con la variabñe
+        self.setPalette(paleta) #establecemos el nuevo color con la paleta
         self.show() ##mostramos la ventana
+        caixa = QVBoxLayout() #esto es un layout
+        boton_ventana_ejemplo_saludo = QPushButton('saludo') #esto sería un boton
+        caixa.addWidget(boton_ventana_ejemplo_saludo) #añadimos el boton al layout
+
+        container = QWidget() #necesitamos hacer un casteo, ya que el metodo setCentralWidget solo admite un QWidget
+        container.setLayout(caixa) #establecemos el layout con la caja
+        self.setCentralWidget(container) #metemos el container convertido en la ventana
 
 
 if __name__ == "__main__":
