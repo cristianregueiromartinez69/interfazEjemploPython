5c8960496ba8e8b3ce5819736e4886b4a2b336ea
Cristian <cregueiromartinez@danielcastelao.org>
2024-10-25 13:00:12 +0200

    primeros metodos ventana

--- InterfacesEjemplo.py
+++ InterfacesEjemplo.py
@@ -4,6 +4,8 @@ aQplication nos permite comunicarnos con el sisema operativo
 '''
 import sys
 
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPalette, QColor
 from PyQt6.QtWidgets import (QMainWindow, QApplication)
 from setuptools.extern import names
 
@@ -11,9 +13,15 @@ from setuptools.extern import names
 class PrimeraVentana(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle('Ventana ejemplo') #este metodo le da un titulo a la ventana
+        self.setFixedSize(800, 600) #este metodo fija el tamaño de la ventana
+        paleta = self.palette()
+        paleta.setColor(QPalette.ColorRole.Window, QColor("purple"))
+        self.setPalette(paleta)
+        self.show() ##mostramos la ventana
 
 
 if __name__ == "__main__":
     aplicacion = QApplication(sys.argv)
     fiestra = PrimeraVentana()
-    aplicacion.exec()
+    aplicacion.exec() #ejecutamos la aplicacion
